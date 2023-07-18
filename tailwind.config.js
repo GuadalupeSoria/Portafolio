57a575e6681a55aae4b3973d7d6d9a6d2d092e34
Guadalupe <guadalupe@flinbo.com>
2023-07-18 12:37:52 +0200

    create app

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      zIndex: {
+        100: '100',
+        200: '200',
+      },
+      fontFamily: {
+        Outfit: ['Outfit', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
